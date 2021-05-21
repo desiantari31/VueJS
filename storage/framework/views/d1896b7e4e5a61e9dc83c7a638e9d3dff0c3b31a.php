f8a3856dd833ab2b85503c6865c77febf8cd382e
desiantari31 <71802098+desiantari31@users.noreply.github.com>
2021-05-21 22:01:17 +0800

    Add files via upload

---
+++ storage/framework/views/d1896b7e4e5a61e9dc83c7a638e9d3dff0c3b31a.php
@@ -0,0 +1,15 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><?php echo e(__('Dashboard')); ?></div>
+
+               <example-component></example-component>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel-vue\resources\views/home.blade.php ENDPATH**/ ?>
